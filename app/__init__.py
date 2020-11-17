03cc3cf1869cf5b97acd3291054911d1bae5bdee
WilliamJiemesha <67468607+WilliamJiemesha@users.noreply.github.com>
2020-11-17 20:49:25 +0800

    Awal

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+#https://zonacoding.com/article/penerapan-mvc-di-flask-python
+
+from flask import Flask ## import Flask dari package flask
+
+app = Flask(__name__)
+
+from app.controllers import  *
