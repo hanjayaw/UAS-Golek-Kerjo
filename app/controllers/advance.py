7d61e327c3e119f004a48c8c2774e25fb5eec499
calvinch31uc <cchandra06@student.ciputra.ac.id>
2020-12-06 01:56:09 +0700

    hasil

--- app/controllers/advance.py
+++ app/controllers/advance.py
@@ -4,4 +4,9 @@ from app import app, RunSelect, ExecuteCMD, RunSelectOne
 
 @app.route('/advance', methods=["POST", "GET"])
 def advance():
+    if request.method == "POST":
+            searching = request.form["search"]
+            qry = "SELECT tipe_job , nama_perusahaan , kota , minimum_gaji , logo_perusahaan FROM jobs , perusahaan , kota WHERE perusahaan.id_kota = kota.id_kota AND jobs.id_perusahaan = perusahaan.id_perusahaan AND tipe_job like \'%"+searching+"%\' OR nama_perusahaan like \'%"+searching+"%\'"
+            results = RunSelect(qry)
+            return render_template('advance.html', results = results)
     return render_template('advance.html')
