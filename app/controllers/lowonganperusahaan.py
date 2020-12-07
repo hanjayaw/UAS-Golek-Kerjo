c0b5695c7fd1963770d69147f706f3b818c18489
hanjayaw <71307231+hanjayaw@users.noreply.github.com>
2020-12-07 23:27:25 +0700

    update fix

--- app/controllers/lowonganperusahaan.py
+++ app/controllers/lowonganperusahaan.py
@@ -42,9 +42,9 @@ def perusahaan():
     else:
         currentindex = 0
     #NAMA PERUSAHAAN, KOTA PERUSAHAAN, BERAPA JUMLAH LOWONGANNYA, LOGO
-    sql = "SELECT p.nama_perusahaan, k.kota, COUNT(p.id_perusahaan) , p.logo_perusahaan from jobs j, kota k, perusahaan p where p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.id_perusahaan LIMIT 6;"
+    sql = "SELECT p.nama_perusahaan, k.kota, COUNT(p.id_perusahaan) , p.logo_perusahaan, p.id_perusahaan from jobs j, kota k, perusahaan p where p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.id_perusahaan LIMIT 6;"
     results = RunSelect(sql)
-    sql = "SELECT p.nama_perusahaan, k.kota, COUNT(p.id_perusahaan) , p.logo_perusahaan from jobs j, kota k, perusahaan p where p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.id_perusahaan LIMIT " + str(
+    sql = "SELECT p.nama_perusahaan, k.kota, COUNT(p.id_perusahaan) , p.logo_perusahaan, p.id_perusahaan from jobs j, kota k, perusahaan p where p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.id_perusahaan LIMIT " + str(
         currentindex) + ", " + str(n_size) + ";"
     hasil = RunSelect(sql)
     return render_template('perusahaan.html',
