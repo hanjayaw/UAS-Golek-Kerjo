2c75a0fde1a1b04a81e9ab150a73cee306bca4b7
WilliamJiemesha <67468607+WilliamJiemesha@users.noreply.github.com>
2020-12-11 20:42:40 +0800

    Fixed perusahaan queries

--- app/controllers/lowonganperusahaan.py
+++ app/controllers/lowonganperusahaan.py
@@ -32,9 +32,9 @@ def lowongan():
 @app.route('/perusahaan', methods=["POST", "GET"])
 def perusahaan():
     currentpage = request.args.get('page', 1)  #langkah 1 tau currentpage
-    sql = "SELECT COUNT(*) as jumlah_perusahaan FROM perusahaan, jobs WHERE perusahaan.id_perusahaan = jobs.id_perusahaan GROUP BY perusahaan.id_perusahaan"
+    sql = "SELECT COUNT(jumlahsemua.jumlah_perusahaan) as jumlah FROM (SELECT COUNT(*) as jumlah_perusahaan FROM jobs GROUP BY jobs.id_perusahaan) as jumlahsemua"
     res = RunSelectOne(sql)  #LANGKAH KE 2 COUNT DATA
-    n_item = res[0][0]
+    n_item = res[0]
     n_size = 6
     n_pages = math.ceil(n_item / n_size)
     if int(currentpage) > 1:
@@ -42,9 +42,10 @@ def perusahaan():
     else:
         currentindex = 0
     #NAMA PERUSAHAAN, KOTA PERUSAHAAN, BERAPA JUMLAH LOWONGANNYA, LOGO
-    sql = "SELECT p.nama_perusahaan, k.kota, COUNT(p.id_perusahaan) , p.logo_perusahaan, p.id_perusahaan from jobs j, kota k, perusahaan p where p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.id_perusahaan LIMIT 6;"
+    sql = "SELECT nama_perusahaan, COUNT(nama_perusahaan), kota, logo_perusahaan, id_perusahaan FROM (SELECT p.nama_perusahaan, k.kota, p.logo_perusahaan, p.id_perusahaan from jobs j, kota k, perusahaan p WHERE p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.auto_num, j.auto_num, j.id_perusahaan) as jumlah_semua GROUP BY jumlah_semua.nama_perusahaan, kota, logo_perusahaan, id_perusahaan LIMIT 6"
     results = RunSelect(sql)
-    sql = "SELECT p.nama_perusahaan, k.kota, COUNT(p.id_perusahaan) , p.logo_perusahaan, p.id_perusahaan from jobs j, kota k, perusahaan p where p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.id_perusahaan LIMIT " + str(
+
+    sql = "SELECT nama_perusahaan, COUNT(nama_perusahaan), kota, logo_perusahaan, id_perusahaan FROM (SELECT p.nama_perusahaan, k.kota, p.logo_perusahaan, p.id_perusahaan from jobs j, kota k, perusahaan p WHERE p.id_kota = k.id_kota and j.id_perusahaan = p.id_perusahaan GROUP BY p.auto_num, j.auto_num, j.id_perusahaan) as jumlah_semua GROUP BY jumlah_semua.nama_perusahaan, kota, logo_perusahaan, id_perusahaan LIMIT " + str(
         currentindex) + ", " + str(n_size) + ";"
     hasil = RunSelect(sql)
     return render_template('perusahaan.html',
