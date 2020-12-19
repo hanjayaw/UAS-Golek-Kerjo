a4e752c9d48dc189e2663365b2b0a761e616bc17
WilliamJiemesha <67468607+WilliamJiemesha@users.noreply.github.com>
2020-12-19 21:28:35 +0800

    Update profile.py

--- app/controllers/profile.py
+++ app/controllers/profile.py
@@ -13,7 +13,8 @@ def before_request():
 @app.route('/profile', methods=["POST", "GET"])
 def profile():
     if "user" in session:
-        sqlutama = "SELECT `nama_pekerja`,`jenis_kelamin`,`umur_pekerja`,`pendidikan_terakhir`,`alamat`,`kota`,`provinsi`,`telepon_pekerja`,`email`,`instagram`,`linkedin`,`tipe_pekerjaan`,`durasi_pekerjaan`,`preferensi_gaji`,`ktp`FROM pekerja,kota,provinsi WHERE pekerja.id_kota = kota.id_kota and pekerja.id_provinsi = provinsi.id_provinsi and id_pekerja = \'"+ session["iduser"].upper() + "\';"
+        sqlutama = "SELECT `nama_pekerja`,`jenis_kelamin`,`umur_pekerja`,`pendidikan_terakhir`,`alamat`,SUBSTRING(kota, LOCATE(' ', kota)+1),`provinsi`,`telepon_pekerja`,`email`,`instagram`,`linkedin`,`tipe_pekerjaan`,`durasi_pekerjaan`,`preferensi_gaji`,`ktp`FROM pekerja,kota,provinsi WHERE pekerja.id_kota = kota.id_kota and pekerja.id_provinsi = provinsi.id_provinsi and id_pekerja = \'" + session[
+            "iduser"].upper() + "\';"
         # return sqlutama
         pekerja = RunSelect(sqlutama)
         sql = ("SELECT `kota`,`id_kota` FROM kota")
@@ -25,14 +26,15 @@ def profile():
         ktp = None
         durationpekerjaan = ['Full Time', 'Part Time', 'Freelance']
         lulusanutama = ['S1', 'S2', 'SMA']
-        genderutama = ['Perempuan','Laki-laki']
-        sqlhistori = ("SELECT `nama_perusahaan` , `nama_pekerja` , `tanggal_upload` FROM pekerjatoperusahaan pj ,pekerja,perusahaan WHERE pj.id_pekerja = pekerja.id_pekerja and pj.id_perusahaan = perusahaan.id_perusahaan and pj.id_pekerja = \'"
+        genderutama = ['Perempuan', 'Laki-laki']
+        sqlhistori = (
+            "SELECT `nama_perusahaan` , `nama_pekerja` , `tanggal_upload` FROM pekerjatoperusahaan pj ,pekerja,perusahaan WHERE pj.id_pekerja = pekerja.id_pekerja and pj.id_perusahaan = perusahaan.id_perusahaan and pj.id_pekerja = \'"
             + session["iduser"].upper() + "\';")
         histori = RunSelect(sqlhistori)
-        member = ("SELECT `jenis_membership` FROM pekerja p, membership m where p.id_membership = m.id_membership and p.id_pekerja = \'"
+        member = (
+            "SELECT `jenis_membership` FROM pekerja p, membership m where p.id_membership = m.id_membership and p.id_pekerja = \'"
             + session["iduser"].upper() + "\';")
         jenismember = RunSelect(member)
-
 
         if request.method == "POST":
             try:
@@ -43,7 +45,7 @@ def profile():
                 saveProfil(profil)
                 pekerja = RunSelect(sqlutama)
                 return redirect(request.url)
-                app.before_request(before_request)                
+                app.before_request(before_request)
             nama = request.form["namaprofil"].upper()
             gender = request.form.get("genderprofil")
             umur = request.form["umur"]
@@ -66,100 +68,104 @@ def profile():
             #qry = 'UPDATE pekerja set '
             if nama != "" and nama != None:
                 session["user"] = nama
-                qry = 'CALL updatenama (\'' + nama + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatenama (\'' + nama + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
 
             if gender != "" and gender != None:
-                qry = 'CALL updatejeniskelamin (\'' + gender + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatejeniskelamin (\'' + gender + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
 
             if umur != "" and umur != None:
-                qry = 'CALL updateumur (\'' + umur + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updateumur (\'' + umur + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
 
             if lulusan != "" and lulusan != None:
                 #qry += ", pendidikan_terakhir = \'" + lulusan + "\'"
-                qry = 'CALL updatelulusan (\'' + lulusan + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatelulusan (\'' + lulusan + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
 
             if alamat != "" and alamat != None:
 
                 #qry += ", alamat = \'" + alamat + "\'"
-                qry = 'CALL updatealamat(\'' + alamat + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatealamat(\'' + alamat + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
 
             if kotanya != "" and kotanya != None:
 
                 #qry += ", id_kota = \'" + kotanya + "\'"
-                qry = 'CALL updatekota(\'' + kotanya + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatekota(\'' + kotanya + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
-                
-
 
             if provinsinya != "" and provinsinya != None:
 
                 #qry += ", id_provinsi = \'" + provinsinya + "\'"
-                qry = 'CALL updateprovinsi(\'' + provinsinya + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updateprovinsi(\'' + provinsinya + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
 
             if notelp != "" and notelp != None:
 
                 #qry += ", telepon_pekerja = \'" + notelp + "\'"
-                qry = 'CALL updatetelepon(\'' + notelp + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatetelepon(\'' + notelp + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
 
             if instagram != "" and instagram != None:
 
                 #qry += ", instagram = \'" + instagram + "\'"
-                qry = 'CALL updateinstagram(\'' + instagram + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updateinstagram(\'' + instagram + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
             if linkedin != "" and linkedin != None:
                 #qry += ", linkedin = \'" + linkedin + "\'"
-                qry = 'CALL updatelinkedin(\'' + linkedin + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatelinkedin(\'' + linkedin + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
 
             if email != "" and email != None:
                 session["email"] = email
-                qry = 'CALL updateemail(\'' + email + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updateemail(\'' + email + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
 
             if ktp.filename != "" and ktp.filename != None:
                 saveKTP(ktp)
             if jobnya != "" and jobnya != None:
                 #qry += ", tipe_pekerjaan = \'" + jobnya + "\'"
-                qry = 'CALL updatetipe(\'' + jobnya + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatetipe(\'' + jobnya + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
 
             if tipejob != "" and tipejob != None:
                 #qry += ", durasi_pekerjaan = \'" + tipejob + "\'"
-                qry = 'CALL updatedurasi(\'' + tipejob + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updatedurasi(\'' + tipejob + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
 
             if gaji != "" and gaji != None:
                 #qry += ", preferensi_gaji = \'" + gaji + "\'"
-                qry = 'CALL updategaji(\'' + gaji + '\'  , \'' + session["iduser"].upper() + '\')'
+                qry = 'CALL updategaji(\'' + gaji + '\'  , \'' + session[
+                    "iduser"].upper() + '\')'
                 ExecuteCMD(qry)
-
 
             #qry += 'WHERE id_pekerja = \'' + session["iduser"].upper() + '\';'
             #ExecuteCMD(qry)
-            pekerja= RunSelect(sqlutama)
+            pekerja = RunSelect(sqlutama)
             return render_template("profile.html",
                                    kota=kota,
                                    provinsi=provinsi,
                                    jobs=jobs,
                                    duration=durationpekerjaan,
                                    lulusannya=lulusanutama,
-                                   genderkerja = genderutama,
-                                   histori = histori,
-                                   jenismember = jenismember,
+                                   genderkerja=genderutama,
+                                   histori=histori,
+                                   jenismember=jenismember,
                                    namapekerja=pekerja[0][0],
                                    genderpekerja=pekerja[0][1],
                                    umurpekerja=pekerja[0][2],
@@ -183,9 +189,9 @@ def profile():
                                    jobs=jobs,
                                    duration=durationpekerjaan,
                                    lulusannya=lulusanutama,
-                                   genderkerja = genderutama,
-                                   histori = histori,
-                                   jenismember = jenismember,
+                                   genderkerja=genderutama,
+                                   histori=histori,
+                                   jenismember=jenismember,
                                    namapekerja=pekerja[0][0],
                                    genderpekerja=pekerja[0][1],
                                    umurpekerja=pekerja[0][2],
